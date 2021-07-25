989d9683976d57d04f36c2d1270b620d80e90561
AariasCR <alanjarias@hotmail.com>
2021-07-24 22:16:29 -0600

    App Completo

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,19 +6,28 @@ import { Country } from '../../interfaces/pais.interface';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [ ]
+  styles: [ 
+    `
+      li {
+        cursor: pointer;
+      }
+    `
+  ]
 })
 export class PorPaisComponent {
 
   termino: string = '';
   hayError: boolean= false;
   paises: Country [] = [];
+  paisesSugeridos: Country [] = [];
+  mostrar: boolean = false;
+
   constructor(private PaisService:PaisService) { }
 
   buscar( termino:string){
     this.hayError = false;
     this.termino = termino;
-
+    this.mostrar = false;
     this.PaisService.buscarPais(termino)
     .subscribe( (paises)=> {
       this.paises = paises;
@@ -31,7 +40,19 @@ export class PorPaisComponent {
   }
 
   sugerencias( termino:string ){
-    this.hayError = false
+    this.hayError = false;
+    this.mostrar = true;
+    this.termino = termino;
+    this.PaisService.buscarPais(termino)
+    .subscribe( (paises)=> this.paisesSugeridos = paises.splice(0,5),
+    error => this.paisesSugeridos.length = 0);
+
   }
 
+  buscarSugerido(termino:string ){
+    this.buscar(termino);
+    this.mostrar = false;
+  }
 }
+
+
--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styles: [
+    `
+      button{
+        margin-right: 5px;
+      }
+    `
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
 
-  constructor() { }
+  regiones : string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  regionActiva : string='';
+  paises: Country [] = [];
+  constructor( private PaisService: PaisService) { }
 
-  ngOnInit(): void {
+  activarRegion(region:string){
+
+    if(region === this.regionActiva){return}
+
+    this.regionActiva = region;
+    this.paises =[];
+    this.obtenePaises();
+  }
+
+  getClaseCss(region:string):string{
+    return (region !== this.regionActiva)? 'btn btn-outline-primary': 'btn-primary';
+  }
+
+  obtenePaises(){
+    this.PaisService.getPaisPorRegion(this.regionActiva)
+    .subscribe(paises =>{ this.paises = paises; console.log(paises) })
   }
 
 }
